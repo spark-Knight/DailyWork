c5eafca425e9ba1c7508c178e6dc1c5d8db965b6
spark-Knight <67824440+spark-Knight@users.noreply.github.com>
2021-12-23 15:46:47 +0530

    Changed close concept

--- VideoDownloader/downloader.py
+++ VideoDownloader/downloader.py
@@ -1,6 +1,6 @@
 import pywhatkit as kit
 import pytube 
-import os
+import keyboard as key
 
 
 print("Name of video You want to Download")
@@ -8,12 +8,11 @@ print("Name of video You want to Download")
 InputName = str(input())
 videoName = kit.playonyt(InputName) 
 # this is statement close chrome beacause it's open the video on chrome
-os.system("TASKKILL /F /im chrome.exe ")   
 youtube = pytube.YouTube(videoName)  
+key.press_and_release("Alt+F4")
 # It's give good resolution of video which want
 video = youtube.streams.get_highest_resolution()  
 print("Downloading video...")
-# Add the path from where you have to download the video
 video.download('C:\\Users\\dna83\\OneDrive\\Desktop\\Your dad\\download files')  
 print("video downloaded successfully")
 
