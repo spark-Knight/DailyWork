84ad3386b6f3adef65aa2f5ea68ca7cc87e012f9
spark-Knight <67824440+spark-Knight@users.noreply.github.com>
2021-12-25 15:45:26 +0530

    changed to function

--- VideoDownloader/downloader.py
+++ VideoDownloader/downloader.py
@@ -3,17 +3,23 @@ import pytube
 import keyboard as key
 
 
-print("Name of video You want to Download")
-# Here we input video name  
-InputName = str(input())
-videoName = kit.playonyt(InputName) 
-# this is statement close chrome beacause it's open the video on chrome
-youtube = pytube.YouTube(videoName)  
-key.press_and_release("Alt+F4")
-# It's give good resolution of video which want
-video = youtube.streams.get_highest_resolution()  
-print("Downloading video...")
-video.download('C:\\Users\\dna83\\OneDrive\\Desktop\\Your dad\\download files')  
-print("video downloaded successfully")
+def downloader():
+    print("Name of video You want to Download")
+    # Here we input video name  
+    InputName = str(input())
+    videoName = kit.playonyt(InputName) 
+    # this is statement close chrome beacause it's open the video on chrome
+    youtube = pytube.YouTube(videoName)  
+    # key.press_and_release("Alt+F4")
+    key.press_and_release("Ctrl+w")
+    # It's give good resolution of video which want
+    video = youtube.streams.get_highest_resolution()  
+    print("Downloading video...")
 
+    video.download('C:\\Users\\dna83\\OneDrive\\Desktop\\Your dad\\download files')  
+    print("video downloaded successfully")
+
+
+
+downloader()
 
